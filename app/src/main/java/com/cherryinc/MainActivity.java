9758b142da3e8cc90702296c066a31a92881f079
denis <025896321Denis>
2020-04-13 22:03:10 +0300

    жд

--- app/src/main/java/com/cherryinc/MainActivity.java
+++ app/src/main/java/com/cherryinc/MainActivity.java
@@ -7,16 +7,21 @@ import android.app.WallpaperManager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    int [] images = {R.drawable.b_1,R.drawable.b_2,R.drawable.b_3, R.drawable.b_4, R.drawable.b_5, R.drawable.b_6};
+    private int imageCaunter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
     }
 
     @SuppressLint("ResourceType")
@@ -30,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
         try {
             // wallpaperManager.setBitmap(bitmap);
-            wallpaperManager.setResource(R.drawable.b_3);
+            wallpaperManager.setResource(images[imageCaunter]);
 
             wallpaperManager.suggestDesiredDimensions(width, height);
             Toast.makeText(
@@ -53,5 +58,16 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void onClickNext(View view) {
+        if(imageCaunter < images.length -1){
+            imageCaunter ++;
+        }else {
+            imageCaunter = 0;
+        }
+
+        imageView.setImageDrawable(getResources().getDrawable(images[imageCaunter]));
+
     }
 }
