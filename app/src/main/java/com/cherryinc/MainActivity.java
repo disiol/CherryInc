f79e9a3d6d37f94a2b30a76241ab3cadae5fc4dd
denis <025896321Denis>
2020-04-13 21:30:10 +0300

    Initial commit

---
+++ app/src/main/java/com/cherryinc/MainActivity.java
@@ -0,0 +1,44 @@
+package com.cherryinc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.WallpaperManager;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("ResourceType")
+    public void onClick(View view) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+// Вычисляем размеры экрана
+        int height = metrics.heightPixels;
+        int width = metrics.widthPixels;
+
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
+        try {
+            // wallpaperManager.setBitmap(bitmap);
+            wallpaperManager.setResource(R.drawable.b_3);
+
+            wallpaperManager.suggestDesiredDimensions(width, height);
+            Toast.makeText(
+                    this,
+                    "Обои установлены",
+                    Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
